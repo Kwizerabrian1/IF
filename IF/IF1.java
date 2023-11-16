3eefce176a01963d6358f739888ae47876b1be8f
Kwizerabrian1 <150962856+Kwizerabrian1@users.noreply.github.com>
2023-11-16 18:36:59 +0200

    Add files via upload

---
+++ IF/IF1.java
@@ -0,0 +1,8 @@
+public class if1 {    
+public static void main(String[] args) {    
+int num = 10;        
+if(num % 2 ==0) {    
+System.out.println("The number is even");    
+}  
+}      
+}   
---
+++ IF/IF2.java
@@ -0,0 +1,8 @@
+public class if2 {    
+public static void main(String[] args) {    
+String country = "FRANCE";        
+if(country.equals("FRANCE")) {    
+System.out.println("THE COUNTRY IS FRANCE!");    
+}  
+}      
+}   
