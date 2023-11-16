5211e4f12aeb7276c0f2bd04e22c7fe659dfa091
Kwizerabrian1 <150962856+Kwizerabrian1@users.noreply.github.com>
2023-11-16 18:41:25 +0200

    Add files via upload

---
+++ IF ELSE/ifelse1.java
@@ -0,0 +1,11 @@
+public class ifelse1 {    
+public static void main(String[] args) {    
+String text="Hello";    
+    
+if(text.isEmpty()) {    
+System.out.println("the string is empty");    
+}  else {  
+    System.out.println("the string is not empty");  
+    }    
+}      
+}   
